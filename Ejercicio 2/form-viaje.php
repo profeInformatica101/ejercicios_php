e3572fa7ca87cdfa15a0588825926ce84997f400
Pablo <pablo.dominguez@iescamas.es>
2024-09-25 23:12:02 +0200

    Ejercicio 2

---
+++ Ejercicio 2/form-viaje.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <form action="calculo-viaje.php" method="post">
+        <label for="distancia">Distancia:</label>
+        <input type="number" name="distancia">
+         <label for="velocidad">Velocidad promedio</label>
+        <input type="number" name="velocidad">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
