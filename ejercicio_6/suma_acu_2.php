b2c3b9a6463b16c278c07f240727317dc26f957e
Juan <jdommay863@g.educaand.es>
2024-10-03 09:15:52 +0200

    Añadido ejercicio 6

---
+++ ejercicio_6/suma_acu_2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        Numero: <input type="number" name="numero" id="">
+        <input type="submit" value="enviar">
+    </form>
+
+    <?php
+/**
+ * Enunciado: Diseña un formulario que pida al usuario un número N. 
+ * l enviar el formulario, 
+ * el programa debe calcular y mostrar la suma de todos 
+ * los números desde 1 hasta N.
+ */
+//var_dump($_GET);
+if(isset($_GET["numero"])){
+    $n = $_GET["numero"];
+    $resultado = 0;
+    $i = 1;
+
+    while($i <= $n){
+        $resultado += $i;
+        $i++;
+    }
+
+
+}
+echo "<h2>$resultado</h2>";
+
+
+?>
+</body>
+</html>
+
